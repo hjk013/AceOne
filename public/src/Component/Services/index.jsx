3f0fff2ef26c614d319c37586cc413a016c17ec0
Hyeon Jun Kim <hjk013@gmail.com>
2019-02-03 20:42:20 -0800

    started service

--- public/src/Component/Services/index.jsx
+++ public/src/Component/Services/index.jsx
@@ -3,7 +3,14 @@ import "./services.scss";
 
 const Services = () => (
   <div id="services">
-    <div>Services!!!!!!!</div>
+    <div className="services-grid-container">
+      <div className="services-grid-item">Grid Item 1 </div>
+      <div className="services-grid-item">Grid Item 2 </div>
+      <div className="services-grid-item">Grid Item 3 </div>
+      <div className="services-grid-item">Grid Item 4 </div>
+      <div className="services-grid-item">Grid Item 5 </div>
+      <div className="services-grid-item">Grid Item 6 </div>
+    </div>
   </div>
 );
 
